de45fbe8eacbf733f354d5b3ca0cf77949495018
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-08-21 12:58:22 +0300

    запущен и выполнен первый поток на заполение списка

--- main3.py
+++ main3.py
@@ -6,12 +6,10 @@ random_len = []
 
 
 def rand(random_len):
-    random_len = [randint(-10, 10) for i in range(10)]
+    random_len = [randint(-10, 10) for _ in range(10)]
     print(random_len)
 
 
-
-# rand(random_len)
-
-
+thread_1 = threading.Thread(target=rand(random_len), args=(random_len,))
+thread_1.start()
 
