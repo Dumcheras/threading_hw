bdb0c5d4c049ccfe6eefbf2289a5d98d53df6639
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-08-21 17:02:48 +0300

    исправлен основной поток, заменен на корректный цикл

--- main3.py
+++ main3.py
@@ -1,15 +1,33 @@
+import time
 from random import randint
 import threading
 from statistics import mean
+from threading import Lock
 
 random_len = []
+lock = Lock()
 
 
 def rand(random_len):
-    random_len = [randint(-10, 10) for _ in range(10)]
+    for i in range(10):
+        i = randint(0, 10)
+        random_len.append(i)
     print(random_len)
 
 
+def summ_list(random_len, lock: Lock):
+    lock.acquire()
+    time.sleep(1)
+    lock.release()
+    print(sum(random_len), end=" ")
+
+
 thread_1 = threading.Thread(target=rand(random_len), args=(random_len,))
+thread_2 = threading.Thread(target=summ_list(random_len, lock), args=(random_len, lock,))
+
 thread_1.start()
+thread_2.start()
+
+thread_1.join()
+thread_2.join()
 
