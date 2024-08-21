e533c186382acf087f68c8ef1408760acc959cd0
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-08-21 12:50:14 +0300

    написана функция, заполняющая список случайными числами

--- main3.py
+++ main3.py
@@ -1,2 +1,17 @@
+from random import randint
 import threading
 from statistics import mean
+
+random_len = []
+
+
+def rand(random_len):
+    random_len = [randint(-10, 10) for i in range(10)]
+    print(random_len)
+
+
+
+# rand(random_len)
+
+
+
