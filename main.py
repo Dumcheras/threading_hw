e551b0a1482e1e9c4588f65b4b5f2917e915b55b
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-08-21 12:24:53 +0300

    добавлен разделитель print

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def func_min(user_list):
 for i in range(5):
     thread_1 = threading.Thread(target=func_max, args=(user_list,))
     thread_1.start()
+print("")
 for i in range(5):
     thread_2 = threading.Thread(target=func_min, args=(user_list,))
     thread_2.start()
