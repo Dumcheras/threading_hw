ab763d6482ad05aad5de104dd4bbc5157fb1d362
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-08-21 12:18:41 +0300

    написана функция func_min для поиска минимума в ряду, запущена поточность в пять циклов

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ user_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 def func_max(user_list):
-    print(max(user_list))
+    print(max(user_list), end=" ")
+
+
+def func_min(user_list):
+    print(min(user_list), end=" ")
 
 
 for i in range(5):
     thread_1 = threading.Thread(target=func_max, args=(user_list,))
     thread_1.start()
+for i in range(5):
+    thread_2 = threading.Thread(target=func_min, args=(user_list,))
+    thread_2.start()
